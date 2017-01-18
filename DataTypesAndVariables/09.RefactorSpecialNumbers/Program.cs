7887a246b4241e93043d7d66e7aeaa9f6f5dd0bb
SvetozarMateev <vidic9898@gmail.com>
2017-01-19 01:32:49 +0200

    Added last two solutions

---
+++ DataTypesAndVariables/09.RefactorSpecialNumbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09.RefactorSpecialNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            bool special = false;
+            for (int i = 1; i <= n; i++)
+            {
+                int number = i;
+
+                int sum = 0;
+
+                while (number > 0)
+                {
+                    sum += i % 10;
+                    number = i / 10;
+                }
+                special = (sum == 5) || (sum == 7) || (sum == 11);
+                Console.WriteLine($"{i} -> {special}");
+                sum = 0;
+                
+            }
+
+        }
+    }
+}
